d1f6cc8a88c1d5a2b16790dc9d3375888f1ce27f
ColeRutledge <ColeRutledge@gmail.com>
2020-08-21 20:17:18 -0400

    add @jwt_required to api/posting

--- app/routes/posting.py
+++ app/routes/posting.py
@@ -1,6 +1,6 @@
 from flask import Flask, Blueprint, jsonify, request
 from flask_jwt_extended import jwt_required
-from sqlalchemy import func, not_, or_
+from sqlalchemy import or_
 from datetime import datetime
 import json
 from app.models import db, Posting
@@ -9,10 +9,12 @@ bp = Blueprint('posting', __name__, url_prefix="/api/posting")
 
 
 @bp.route('/', strict_slashes=False, methods=['GET'])
+@jwt_required
 def get_posts():
   print()
   print('********FETCH POSTINGS********')
   print()
+
   conditions = [
       '.net',
       'java ',
@@ -29,7 +31,7 @@ def get_posts():
       'director',
   ]
 
-  filters = [Posting.title.ilike(f'%{term}%') for term in conditions]
+  filters = [Posting.title.ilike(f'%{condition}%') for condition in conditions]
   postings = Posting.query.filter(~or_(*filters))
 
   res = [{
@@ -45,4 +47,5 @@ def get_posts():
       'description': posting.description,
       'link': posting.link,
   } for posting in postings]
+
   return jsonify(res), 200
