1abbb585877e968a70acd344481fe3926d50c0cd
ColeRutledge <ColeRutledge@gmail.com>
2021-05-05 14:08:58 -0400

    change webserver to gunicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 pip3 install -r /app/requirements.txt
 python3 entry.py db upgrade
 python3 db_seed.py
-python3 entry.py run -h 0.0.0.0
-# gunicorn app:app
+# python3 entry.py run -h 0.0.0.0
+gunicorn app:app
