6c46a3aebc2ff4e38c1573525440eecb1be4000d
ColeRutledge <ColeRutledge@gmail.com>
2021-07-08 13:36:26 -0400

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ pip3 install -r /app/requirements.txt
 python3 entry.py db upgrade
 python3 db_seed.py
 # python3 entry.py run -h 0.0.0.0:$PORT
-gunicorn -b :$PORT app:app
+gunicorn -b 0.0.0.0:$PORT app:app
